64b0c858f4c0a3048ed4a5e5a87e0d6072c8c738
worldrichzhang <worldrichzhang@gmail.com>
2025-10-12 11:41:34 +0800

    fix: 移动端点击menu后先关闭再跳转

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -67,6 +67,7 @@ export default function Header({ data }: HeaderProps) {
                     href={item.href}
                     key={item.name}
                     className="text-gray-700 hover:text-blue-600 transition-colors"
+                    onClick={() => setIsMenuOpen(false)}
                   >
                     {item.name}
                   </a>
